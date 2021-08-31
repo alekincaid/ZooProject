a4d6421a06ceba43fc20a7419b5225d0cf2a418f
alekincaid <89851302+alekincaid@users.noreply.github.com>
2021-08-31 08:50:56 -0500

    Add files via upload

---
+++ Lion.java
@@ -0,0 +1,18 @@
+
+/**
+ * Lion says "Roar!"
+ *
+ * @author Alejandro Kincaid
+ * @version 1.1
+ */
+public class Lion
+{
+  
+    private int age;
+    
+    public void roar()
+    {
+        System.out.println("Roar! Roar!");
+    } 
+    
+} 
---
+++ ZooProgram.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class ZooProgram here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class ZooProgram
+{
+    public static void main (String [] args) 
+    {
+        Lion leo = new Lion ();
+        leo.roar();
+        
+        
+        
+        
+    } //main () end
+} //ZooProgram end
